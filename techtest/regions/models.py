7cedc060860cf048eafb63da82d1c9e626394d0c
surajit003 <surajit.das0320@gmail.com>
2022-03-16 11:37:33 +0300

    add _str_ to region

--- techtest/regions/models.py
+++ techtest/regions/models.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 
 class Region(models.Model):
     code = models.CharField(max_length=2, unique=True)
     name = models.CharField(max_length=255)
-    
+
+    def __str__(self):
+        return f"Region# {self.code} {self.name}"
